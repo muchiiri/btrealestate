e834e2a2f9f1f121abbf7df010a51dac1acd1e54
only1Migwi <charliemigwi@gmail.com>
2022-12-28 17:42:40 +0300

    created Realtor model

--- realtors/models.py
+++ realtors/models.py
@@ -7,7 +7,7 @@ class Realtor(models.Model):
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
-    is_mvp = models.BoolField(defaul=False)
+    is_mvp = models.BooleanField(defaul=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.name
