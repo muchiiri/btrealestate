acf5c496c5f55a265999f162bc2030cd5749caa5
only1Migwi <charliemigwi@gmail.com>
2022-12-28 19:57:32 +0300

    added media configurations

--- btre/settings.py
+++ btre/settings.py
@@ -137,3 +137,7 @@ print(STATIC_DIR)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
