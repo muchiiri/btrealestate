b72defb5822f67d8ca945e3dcc46b849c9145cd0
only1Migwi <charliemigwi@gmail.com>
2022-12-29 20:44:00 +0300

    render listings to listings template

--- listings/views.py
+++ listings/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from . models import Listing
 
 def index(request):
-    listings = Listing.objects.all()
+    # listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
     context = {
         'listings': listings
     }
     return render(request, 'listings.html', context)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request, 'listing.html')
 
 def search(request):
