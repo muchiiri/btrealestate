e697814be3b36834dee5439f685f3523b1f93b40
only1Migwi <charliemigwi@gmail.com>
2022-12-29 12:17:38 +0300

    admin fields customization

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from . models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'hire_date', 'email', 'is_mvp')
+    list_display_links =('id', 'name')
+    list_filter = ('name',)
+    list_editable = ('is_mvp',)
+    search_fields = ('name',)
+    list_per_page = 10
+
+admin.site.register(Realtor, RealtorAdmin)
